6f6bd5d823ed38b3fd2b75987b21174b0a354f21
DB4SCW <info@erklaeranlage.de>
2025-04-14 12:51:29 +0000

    add VB2CAP

--- index.php
+++ index.php
@@ -73,6 +73,7 @@
                         <ul>
                           <li><span class="flag-icon flag-icon-us flag-icon-squared"></span><a href="https://qrz.com/db/K4B" style="color: black; padding-left: 5px;">K4B</a></li> <!-- KO4FX -->
                           <li><span class="flag-icon flag-icon-ca flag-icon-squared"></span><a href="https://qrz.com/db/VB2CATGIRL" style="color: black; padding-left: 5px;">VB2CATGIRL</a></li> <!-- VA2EMZ -->
+                          <li><span class="flag-icon flag-icon-ca flag-icon-squared"></span><a href="https://qrz.com/db/VB2CAP" style="color: black; padding-left: 5px;">VB2CAP</a></li> <!-- VA2EPR -->
                           <li><span class="flag-icon flag-icon-us flag-icon-squared"></span><a href="https://qrz.com/db/K0K" style="color: black; padding-left: 5px;">K0K</a></li> <!-- KO4ZMC -->
                         </ul>
                       </p>
